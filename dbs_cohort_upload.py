89fb7fe0349f35de23e1c48be1370b87447fc2aa
shivkubrick <shivambhatnagar@GEL-C02ZL7RNMD6N.local>
2021-07-22 16:22:06 +0100

    wip: automate seq

--- dbs_cohort_upload.py
+++ dbs_cohort_upload.py
@@ -12,20 +12,6 @@ engine_db = create_engine('postgresql+psycopg2://postgres:genomicsdb@localhost/d
 engine_index = create_engine('postgresql+psycopg2://cdt_user:Daws0n_H&ll@cln-cdt-ddf-index-01.gel.zone/cohorts')
 engine_dams = create_engine('postgresql+psycopg2://cdt_user:Daws0n_H&ll@cln-cdt-dams-postgresql-01.gel.zone/labkey')
 
-# DAMS Test
-# sql_file = 'dams.sql'
-
-# with engine_dams.connect() as conn:
-#     file = open("dams.sql")
-#     query = text(file.read())
-#     result = conn.execute(query)
-
-# Create DataFrame
-
-# df_dict = [{column: value for column, value in row.items()} for row in result]
-
-# df = pd.DataFrame(df_dict)
-
 # From CSV Direct
 # csv_path = r'./request_data/mild_request_file.csv'
 # df = pd.read_csv(csv_path)
@@ -38,25 +24,32 @@ with engine_db.connect() as c:
 df_dict = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy]
 df = pd.DataFrame(df_dict)
 
+# Get seq no
+
+with engine_index.connect() as c:
+    resultproxy = c.execute("SELECT max(file_sequence_number) from dbs.batch_request_tracker")
+
+temp = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy]
+seq_dict = temp[0]
 
 # Create Header and Footer
 cur_datetime = '140000' + str(datetime.today().strftime('%Y%m%d'))
 org_code = '8J834'
 df_len = str(df.shape[0])
 num_records = df_len.zfill(6)
-#prev_seq = result_dict['max']
-prev_seq = 1
+prev_seq = seq_dict['max']
 seq_no = str(prev_seq + 1)
 seq_no_raw = seq_no
 seq_no = seq_no.zfill(8)
 
 df_cols = df.shape[1]
-print(df.shape[0])
 header_row = []
 footer_row = []
 for i in range(df_cols):
     header_row.append('')
     footer_row.append('')
+
+
 
 header_row_first = '0001011' + org_code + '         DBS           ' + cur_datetime + seq_no + num_records
 footer_row_first = '990101' + org_code + '         DBS           ' + cur_datetime + seq_no + num_records
@@ -83,12 +76,12 @@ base_path = '/tmp/batch_request_process/'
 test_path = './'
 curr_date = datetime.today().strftime('%Y%m%d')
 ftype = '.csv'
-seq_no = 83
+
 
 fname = base_path + str(curr_date) + str(seq_no) + '_dbs_batch_request' + ftype
 test_fname = test_path + str(curr_date) + str(seq_no) + '_dbs_batch_request' + ftype
 df_out = df_out.replace(np.nan, '', regex=True)
-df_out.to_csv(fname, header=False, index=False)
+df_out.to_csv(test_fname, header=False, index=False)
 
 # Updating request tracker
 
